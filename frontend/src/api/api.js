135cb7a31db46e9c36b03695b2244f4464a266da
Gia Chien <giachien616@gmail.com>
2026-03-21 19:23:03 +0700

    fix: correct API_BASE_URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // 🔥 FIX CỨNG API (production chạy luôn)
-export const API_BASE_URL = "https://quiz-platform-sm9a.onrender.com/api/login";
+export const API_BASE_URL = "https://quiz-platform-sm9a.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
